d5028c977548a2023ed019be22490ea3aeb73433
Adrian Salceanu <e@essenciary.com>
2021-12-14 22:16:06 +0100

    Bootstrap to support JuliaHub

--- bootstrap.jl
+++ bootstrap.jl
@@ -1,4 +1,4 @@
-pwd() == joinpath(@__DIR__, "bin") && cd(@__DIR__) # allow starting app from bin/ dir
+(pwd() != @__DIR__) && cd(@__DIR__) # allow starting app from bin/ dir
 
 using WhatsYourNumber
 push!(Base.modules_warned_for, Base.PkgId(WhatsYourNumber))
